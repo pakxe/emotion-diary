7536b67d4a04e87e194a0dff6045ba9caf5810db
pakxe <pigkill40@naver.com>
2023-01-11 03:12:01 +0900

    feat(Header): Header 컴포넌트 완성

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,43 @@
+import styled, { css } from 'styled-components';
+
+const HeaderContainer = styled.div`
+  display: flex;
+  align-items: center;
+  padding: 20px 0;
+  border-bottom: 1px solid #e2e2e2;
+`;
+
+const Title = styled.div`
+  display: flex;
+  justify-content: center;
+  width: 50%;
+`;
+
+const Child = styled.div`
+  ${(props) => {
+    switch (props.direction) {
+      case 'left':
+        return css`
+          justify-content: start;
+        `;
+      case 'right':
+        return css`
+          justify-content: end;
+        `;
+      default:
+        return '';
+    }
+  }}
+  display: flex;
+  width: 25%;
+`;
+
+const Header = ({ left, right, title }) => (
+  <HeaderContainer>
+    <Child direction='left'>{left}</Child>
+    <Title>{title}</Title>
+    <Child direction='right'>{right}</Child>
+  </HeaderContainer>
+);
+
+export default Header;
